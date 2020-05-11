d9279634d5e29ba599e55629caee76923a6a2f6c
Ben Wu <benjxwu@stanford.edu>
2020-05-10 18:25:25 -0700

    Updated home page with animation + links

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './css_components/home.css'
 
 export default class Home extends Component{
 
     render(){
+
         return(
             <div className={'home-background'}>
                 <div className={'home-content home-animation'}>
-                    <span className={'home-animation'}>Software Engineer.</span>
-                    <span className={'home-animation'}> Esports. </span>
-                    <span className={'home-animation'}> Life.</span>
+                    <Link to="/Software_Intro" className={'home-animation'}>Software Engineer. </Link>
+                    <Link to="/Esports_Intro" className={'home-animation'}>Esports. </Link>
+                    <Link to="/" className={'home-animation'}>Life.</Link>
                 </div>
             </div>
         );
