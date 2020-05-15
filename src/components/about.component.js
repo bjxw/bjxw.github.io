876d8e94c38cd0695ef8ff6010a575771b6c23f8
Ben Wu <benjxwu@stanford.edu>
2020-05-15 16:44:08 -0700

    Added designs to About and Software Intro pages

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import './css_components/about.css';
+import Profile from '../images/Profile.jpg'
 
 export default class About extends Component{
 
     render(){
         return(
-            <div>
-                Hey there! Welcome to BJXW's website, a React webapp, containing all of Ben Wu's (my) portfolios, hobbies, etc. in a nutshell.
+            <div className={"about-background"}>
+                <div className={"about-content"}>
+                    <p>
+                        Hey there!
+                    </p>
+                    <br/>
+                    <p>
+                        My name is Ben Wu. I am a software engineer and am also very fond of esports and gaming.
+                    </p>
+                    <br/>
+                    <p>
+                        This is my website showcasing these two particular focuses while also giving an inside look as to what makes me tick.
+                        Keep in mind, some contents are my personal views or solely for entertainment purposes and do not fully reflect my character all the time.
+                    </p>
+                    <br/>
+                    <p>
+                        Welcome!
+                    </p>
+                    <br/>
+                    <img src={Profile} alt={"Ben Wu"} className="avatar"/>
+                </div>
             </div>
         );
     }
--- src/components/esports-intro.component.js
+++ src/components/esports-intro.component.js
@@ -8,7 +8,7 @@ export default class EsportsIntro extends Component{
                 <div className={'esports-content'}>
                 Welcome to my esports section! Esports and video games have been a huge part of my life as a hobby and passion. Growing up I was constantly surrounded by games
                 with a a strong influence from Nintendo franchises, years with a Playstation 2, and free PC and flash games in my childhood. In fact, I initially 
-                majored in computer science with hopes that I would eventually become a game developer.  My first competitive games were Super Smash Bros 64 and Pokemon where I fell in 
+                majored in computer science thinking that I would eventually become a game developer.  My first competitive games were Super Smash Bros 64 and Pokemon where I fell in 
                 love with games that involved steep learning curves in mechanics or strategy. I eventually moved onto DotA 2 in highschool which quickly became my addiction at around 
                 1300 hours just under 2 years. I was involved with Stanford Video Game Association's 2018 rebranding during my time as an undergraduate and only got more involved with 
                 the esports community as time went on. By my senior year, I was part of NVIDIA's GeForce Student Beta program. I'm currently still serving as a senior officer of 
