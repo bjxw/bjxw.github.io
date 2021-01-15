4c0d11b0a1425961fb729ae411ff33cc05faa212
Ben Wu <benjxwu@stanford.edu>
2021-01-15 15:27:07 -0800

    Updates

---
+++ precache-manifest.49e209d04394327d98d9f1721ca3ceac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d001ea0bbc44f2a0c3d4d332bde6c38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14457c0b0247a22e3a4e",
+    "url": "/static/css/2.fe51dd2d.chunk.css"
+  },
+  {
+    "revision": "18dd9d845d871d1af046",
+    "url": "/static/css/main.3ec35d27.chunk.css"
+  },
+  {
+    "revision": "14457c0b0247a22e3a4e",
+    "url": "/static/js/2.9512193c.chunk.js"
+  },
+  {
+    "revision": "2927c17f155caba9c734acdc50e6f829",
+    "url": "/static/js/2.9512193c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18dd9d845d871d1af046",
+    "url": "/static/js/main.18baf55e.chunk.js"
+  },
+  {
+    "revision": "a82a0402ef4a3186c7a8",
+    "url": "/static/js/runtime-main.962244ee.js"
+  },
+  {
+    "revision": "5aa654efaef87764120c97cc5aa5f5b0",
+    "url": "/static/media/CV.5aa654ef.pdf"
+  },
+  {
+    "revision": "46c0c7f7b90ae6469bb3543f06d2cfa3",
+    "url": "/static/media/Oval.46c0c7f7.jpg"
+  },
+  {
+    "revision": "84dd5602fca59659eb651222bf019daf",
+    "url": "/static/media/Profile.84dd5602.jpg"
+  }
+]);
